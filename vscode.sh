9fcbcbf1fd836603be659eff2e8ce2e06cdc2f78
Felipe M A B Huinka <83553140+birksnet@users.noreply.github.com>
2021-10-12 18:25:07 -0300

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -5,13 +5,13 @@
 echo " Instalando ---> Visual Stidio Code ....";
 
 # importando Repositorio
-rpm --import -	-v --nofiles --quiet https://packages.microsoft.com/keys/microsoft.asc;
+sudo rpm --import -	-v --nofiles --quiet https://packages.microsoft.com/keys/microsoft.asc;
 
 
 # Abilitando repositorio 
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo';
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo';
 
 # Atualizando e Instalando Visual Studio code
-dnf  check-update;
-dnf install -q code;
+sudo dnf  check-update;
+sudo dnf install -q code;
 
