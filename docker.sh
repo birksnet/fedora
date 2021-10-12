b99ba697b315351083df8899bafb480be8394192
Felipe M A B Huinka <83553140+birksnet@users.noreply.github.com>
2021-10-12 18:21:51 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,11 +12,26 @@ dnf -yq install  dnf-plugins-core;
 dnf config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo;
+    
+# Abilita Nightly
+dnf config-manager --set-enabled docker-ce-nightly
+
+# Abilita Test
+dnf config-manager --set-enabled docker-ce-test
+    
 
 # Instalando Docker
 dnf install -q docker-ce docker-ce-cli containerd.io;
 
 # Instalando Compose
 dnf install -q docker-compose;
+
+groupadd docker
+
+usermod -aG docker $USER
+
+chown "$USER":"$USER" /home/"$USER"/.docker -R
+chmod g+rwx "$HOME/.docker" -R
+
 
 
