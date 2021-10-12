7181b76f6cb28aaeae013057828a040877bf2616
Felipe M A B Huinka <83553140+birksnet@users.noreply.github.com>
2021-10-12 20:42:17 -0300

    Add files via upload

---
+++ install-m2.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+###### Script para clonar FIRESTORE magenteiro
+###### ANTES DE USAR ESSE ARQUIVO DE PERMISSAO ( sudo chmod a+x /install-m2.sh )
+
+echo "############## Instalando Magento FireStore ####################";
+
+echo "De um ome para o diretorio raiz: " ;
+read PASTA ;
+
+echo " -- > Criando pasta ...";
+        sudo mkdir /$PASTA 
+echo " -- > Aplicando Permissoes ...";
+        sudo chmod 777 /$PASTA 
+
+echo "
+            ----> Clonando Repositorio MagenteiroFireStore
+";
+        sudo git clone --recurse-submodule https://github.com/magenteiro/firestore.git /"$PASTA"/magento2
+
+echo "-------------------  ".$(PWD)."DIRETORIO CLONADO PARA /"$PASTA"/magento2 -------------------------";
+
+echo " -- > Aplicando Permissoes COMPOSES ...";
+        sudo chmod 777 /$PASTA/magento2/docker-compose-dev.yml
+        sudo chmod 777 /$PASTA/magento2/src/composer.json
+
+echo " ------------------- FIM FAZER ALTERAÇÕES ADEQUADAS  /"$PASTA"/magento2";
