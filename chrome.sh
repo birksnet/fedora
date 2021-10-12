4a0d4fa20a45003d3328d3b9bcbb154a8bac46c0
Felipe M A B Huinka <83553140+birksnet@users.noreply.github.com>
2021-10-12 18:24:28 -0300

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -5,11 +5,11 @@
 echo " Instalando ---> GOOGLE CHROME ....";
 
 # Abilitando Repositorios de Terceiros
-dnf install -q fedora-workstation-repositories;
+sudo dnf install -q fedora-workstation-repositories;
 
 # abilita o repositorio do google crome
-dnf config-manager --set-enabled google-chrome;
+sudo dnf config-manager --set-enabled google-chrome;
 
 # Instala a Versão Intavel do Google Crome
-dnf install -q google-chrome-stable -y;
+sudo dnf install -q google-chrome-stable -y;
 
