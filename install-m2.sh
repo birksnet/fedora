62ab30c03797dcc15fc700beb8e50ed49e643b1f
birksnet <birksnet@gmail.com>
2021-10-13 10:39:08 -0300

    comentarios

--- install-m2.sh
+++ install-m2.sh
@@ -18,11 +18,11 @@ echo "
 ";
         sudo git clone --recurse-submodule https://github.com/magenteiro/firestore.git /"$PASTA"/magento2
 
-echo "------------------- DIRETORIO CLONADO PARA /"$PASTA"/magento2 -------------------------";
+echo "------------- DIRETORIO CLONADO PARA /"$PASTA"/magento2 --------------";
 
 echo " -- > Aplicando Permissoes COMPOSES ...";
-        sudo chmod 777 /$PASTA/magento2/docker-compose.dev.yml
-        sudo chmod 777 /$PASTA/magento2/src/composer.json
-        sudo chmod 777 /$PASTA/magento2/src/auth.json.sample
+        #sudo chmod 777 /$PASTA/magento2/docker-compose.dev.yml
+        #sudo chmod 777 /$PASTA/magento2/src/composer.json
+        #sudo chmod 777 /$PASTA/magento2/src/auth.json.sample
 
 echo " ------------------- FIM FAZER ALTERAÇÕES ADEQUADAS  /"$PASTA"/magento2";
