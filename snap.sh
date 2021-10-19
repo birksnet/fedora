8971bf2e15a6e796c20254a0f253ad067aba55b5
Felipe Birk <felipe@mixtelatacado.com.br>
2021-10-19 10:04:51 -0300

    Novo Snap com install

--- snap.sh
+++ snap.sh
@@ -11,4 +11,11 @@ sudo dnf install snapd
 # Abilitando repositorio 
 sudo ln -s /var/lib/snapd/snap /snap
 
+#Instalar Discord
+sudo snap install discord
 
+#Instalar Telegram
+sudo snap install telegram-desktop
+
+#Instalar Discord
+sudo snap install thunderbird
